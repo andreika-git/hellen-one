a43e55da84aaa0c93bcfa021f46b69fa9b28841a
andreika-git <andrei.home@gmail.com>
2022-02-11 12:32:01 +0200

    a few new board-ids

--- board_id/test.sh
+++ board_id/test.sh
@@ -2,4 +2,8 @@
 
 #python3 gen_hellen_board_id_resistors.py 
 
-python3 gen_hellen_board_id.py 72_NB2-e
+#python3 gen_hellen_board_id.py 72_NB2-e
+#python3 gen_hellen_board_id.py 81hd-a
+#python3 gen_hellen_board_id.py alpha2ch-b
+#python3 gen_hellen_board_id.py alpha4ch-a
+
