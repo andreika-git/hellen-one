d155feb80f4936b4f1b27a6e6550f683da545d2d
andreika-git <andrei.home@gmail.com>
2022-03-17 20:49:38 +0200

    more board IDs

--- board_id/test.sh
+++ board_id/test.sh
@@ -6,4 +6,7 @@
 #python3 gen_hellen_board_id.py 81hd-a
 #python3 gen_hellen_board_id.py alpha2ch-b
 #python3 gen_hellen_board_id.py alpha4ch-a
+#python3 gen_hellen_board_id.py alpha2ch-c
+#python3 gen_hellen_board_id.py nb1-c
+#python3 gen_hellen_board_id.py na6_94-c
 
