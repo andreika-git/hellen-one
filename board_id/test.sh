cbba18b19f0221c803aad1ef959db00f389971de
andreika-git <andrei.home@gmail.com>
2022-04-14 09:25:52 +0300

    board_id_154hyundai-c

--- board_id/test.sh
+++ board_id/test.sh
@@ -10,4 +10,5 @@
 #python3 gen_hellen_board_id.py nb1-c
 #python3 gen_hellen_board_id.py na6_94-c
 #python3 gen_hellen_board_id.py nissan121-c
-python3 gen_hellen_board_id.py alpha8ch-a
+#python3 gen_hellen_board_id.py alpha8ch-a
+python3 gen_hellen_board_id.py 154hyundai-c
