0e1afc32efba4949aedc41a6467ffb92d9ee6cd0
andreika-git <andrei.home@gmail.com>
2022-05-18 08:31:26 +0300

    test board

---
+++ tests/step2_copy_hellen1test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd ..
+python3 ./bin/copy_from_Kicad.py "frames:hellen" "tests" "../../gerber" "1test" "a"
+
+echo "Done!"
---
+++ tests/step3_create_hellen1test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd ..
+sh bin/create_board_with_prefix.sh "hellen" "tests" "1test" "a" "bom_replace_hellen1test-a.csv" "0,0"
