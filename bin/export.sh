8f6199be90897a93f0c63d1e1191876a463aff39
David Holdeman <dave@holdemanenterprises.com>
2023-03-16 20:37:51 -0500

    negate x for pos

--- bin/export.sh
+++ bin/export.sh
@@ -22,7 +22,7 @@ python "$DIR/fill-zones.py" "$IN.kicad_pcb"
 
 kicad-cli pcb export gerbers --disable-aperture-macros -l "F.Cu,B.Cu,F.Paste,B.Paste,F.SilkS,B.SilkS,F.Mask,B.Mask,Edge.Cuts,In2.Cu,In1.Cu" --no-x2 --aux-origin "$IN.kicad_pcb" -o gerber/
 kicad-cli pcb export drill --map-format ps --drill-origin plot --excellon-zeros-format suppressleading "$IN.kicad_pcb" -o gerber/
-kicad-cli pcb export pos --format csv --units mm --use-drill-file-origin "$IN.kicad_pcb" -o "gerber/$IN-all-pos.csv"
+kicad-cli pcb export pos --format csv --units mm --use-drill-file-origin --bottom-negate-x "$IN.kicad_pcb" -o "gerber/$IN-all-pos.csv"
 
 python "$DIR/export-vrml.py" "$IN.kicad_pcb" "$X" "$Y" "gerber/$IN.wrl"
 
