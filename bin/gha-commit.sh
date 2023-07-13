4f721588cbf1433008ad28f0c210a3a5f508b45e
David Holdeman <dave@holdemanenterprises.com>
2023-07-12 23:00:02 -0500

    reset kicad_pro file

--- bin/gha-commit.sh
+++ bin/gha-commit.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-
-          git config --local user.email "action@github.com"
-          git config --local user.name "GitHub create-board Action"
-          git add boards/*
-          git status
-          OUT=$(git commit -am "[skip actions] Auto-generated board" 2>&1) || echo "commit failed, finding out why"
-          if echo "$OUT" | grep 'nothing to commit'; then
-            echo "headers: looks like nothing to commit"
-            echo "::set-env name=NOCOMMIT::true"
-            exit 0
-          elif echo "$OUT" | grep 'changed'; then
-            echo "headers: looks like something has changed"
-            exit 0
-          else
-            echo "headers: looks like something unexpected"
-            exit 1
-          fi
+git config --local user.email "action@github.com"
+git config --local user.name "GitHub create-board Action"
+git add boards/*
+git reset HEAD *.kicad_pro
+git status
+OUT=$(git commit -am "[skip actions] Auto-generated board" 2>&1) || echo "commit failed, finding out why"
+if echo "$OUT" | grep 'nothing to commit'; then
+  echo "headers: looks like nothing to commit"
+  echo "::set-env name=NOCOMMIT::true"
+  exit 0
+elif echo "$OUT" | grep 'changed'; then
+  echo "headers: looks like something has changed"
+  exit 0
+else
+  echo "headers: looks like something unexpected"
+  exit 1
+fi
 
