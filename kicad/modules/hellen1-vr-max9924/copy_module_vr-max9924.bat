78ab26fc6438bce93f579e1b40e6b1b42ee6ea01
Vladimir Vinogradov <qwerty-off@mail.ru>
2023-07-25 20:39:26 +0300

    vr-max9924 hall support

--- kicad/modules/hellen1-vr-max9924/copy_module_vr-max9924.bat
+++ kicad/modules/hellen1-vr-max9924/copy_module_vr-max9924.bat
@@ -1,3 +1,3 @@
 cd ..\..\..
 
-python ./bin/copy_from_Kicad.py "modules" "kicad/modules" "gerber" "vr-max9924" "0.2"
+python ./bin/copy_from_Kicad.py "modules" "kicad/modules" "gerber" "vr-max9924" "0.1"
