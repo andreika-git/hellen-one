07318a0d03a3baede007fa8f55278fd1484bdfab
rusefillc <sdfsdfqsf2334234234>
2023-12-10 21:33:44 -0500

    working on unintended *.kicad_pro push #351

--- bin/gha-commit.sh
+++ bin/gha-commit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git config --local user.email "action@github.com"
-git config --local user.name "GitHub create-board Action v2"
+git config --local user.name "GitHub create-board Action"
 echo "Status 1/3"
 git status
 git restore *.kicad_pro
@@ -10,7 +10,7 @@ git add gerber/*
 git add boards/*
 echo "Status 3/3"
 git status
-OUT=$(git commit -am "[skip actions] Auto-generated board" 2>&1) || echo "commit failed, finding out why"
+OUT=$(git commit -am "[skip actions] Auto-generated board v2.1" 2>&1) || echo "commit failed, finding out why"
 if echo "$OUT" | grep 'nothing to commit' || echo "$OUT" | grep 'nothing added to commit'; then
   echo "headers: looks like nothing to commit"
 # todo: NOCOMMIT is deprecated I guess? YESCOMMIT works better with multiple invocations per action
