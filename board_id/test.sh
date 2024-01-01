871f50cf3306a87f2da28e4757c7c901288ed5d4
rusefillc <sdfsdfqsf2334234234>
2024-01-01 13:23:17 -0500

    more IDs

--- board_id/test.sh
+++ board_id/test.sh
@@ -54,6 +54,8 @@
 #python3 gen_hellen_board_id.py h112-mg1-b
 
 python3 gen_hellen_board_id.py hd81-a
+python3 gen_hellen_board_id.py uaefi-b
+python3 gen_hellen_board_id.py gdi4chan-a
 
 # do not forget to manually add new files into git
 # todo: one day make this a github action?
