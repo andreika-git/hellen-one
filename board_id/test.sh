97c8e5c145a32140f0515fe69afdebf32ef22682
Vladimir Vinogradov <qwerty-off@mail.ru>
2024-06-03 21:38:22 +0300

    uaefi121-a ID

--- board_id/test.sh
+++ board_id/test.sh
@@ -59,7 +59,8 @@
 #python3 gen_hellen_board_id.py alphax_bronze-a
 #python3 gen_hellen_board_id.py hd81-b
 #python3 gen_hellen_board_id.py 154hyundai-e
-python3 gen_hellen_board_id.py h112-mg1-c
+#python3 gen_hellen_board_id.py h112-mg1-c
+python3 gen_hellen_board_id.py uaefi121-a
 
 # do not forget to manually add new files into git
 # todo: one day make this a github action?
