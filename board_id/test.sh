30187735b3e3f834473f5275fffc6d7b14963597
Vladimir Vinogradov <qwerty-off@mail.ru>
2025-02-15 00:05:21 +0300

    hd81-e ID

--- board_id/test.sh
+++ board_id/test.sh
@@ -68,7 +68,8 @@
 #python3 gen_hellen_board_id.py alphax_silver-b
 #python3 gen_hellen_board_id.py chuma-a
 #python3 gen_hellen_board_id.py alphax_gold-a
-python3 gen_hellen_board_id.py gm-e92-a
+#python3 gen_hellen_board_id.py gm-e92-a
+python3 gen_hellen_board_id.py hd81-e
 
 # do not forget to manually add new files into git
 # todo: one day make this a github action?
