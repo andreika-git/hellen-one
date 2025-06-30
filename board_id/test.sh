2dc530496b9bd0ff6a9aaf24960021f31cd3c1e2
Vladimir Vinogradov <qwerty-off@mail.ru>
2025-06-30 15:15:00 +0300

    e38-b ID 408

--- board_id/test.sh
+++ board_id/test.sh
@@ -75,7 +75,8 @@
 #python3 gen_hellen_board_id.py hw-nano-a
 #python3 gen_hellen_board_id.py uaefi-pro-e
 #python3 gen_hellen_board_id.py super-uaefi-a
-python3 gen_hellen_board_id.py 135subaru2011-b
+#python3 gen_hellen_board_id.py 135subaru2011-b
+python3 gen_hellen_board_id.py gm-e38-b
 
 # do not forget to manually add new files into git
 # todo: one day make this a github action?
